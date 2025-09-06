8b03598e9678914a22f91bd2a338707af9874a88
Enzo Acosta <enzo.acosta@estudiantes.utec.edu.uy>
2025-09-06 20:39:53 -0300

    Se arreglo el comportamiento incorrecto del boton.

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 const contenedor = document.getElementById("contenedor");
+const btnSaludar = document.getElementById("btnSaludar");
 
 contenedor.addEventListener("click", () => {
     alert("Hola! Soy el div.");
+});
+
+btnSaludar.addEventListener("click", (evento) => {
+    alert("Hola!");
+    evento.stopPropagation(); // Evita que el evento se accione en el div
 });
